17f888409625dc8fc7ea00ac66b6d0c6352ce6b9
Yulia N <iuliia_nak@gm.com>
2021-11-08 13:12:22 +0200

    Bus

--- car_classes/Bus.php
+++ car_classes/Bus.php
@@ -2,5 +2,17 @@
 
 class Bus extends NewCar
 {public const TYPE='Автобусы';
+    protected $maxPassenger;//максимально возможная пассажиронагруженность
 
+    public function transfer($maxPassenger){
+        $this->setmaxPassenger($maxPassenger);
+        return self::TYPE . ": Максимально допустимое количество пассажиров = " . $this->maxPassenger."<br>";
+    }
+    public function setmaxPassenger($gruz){
+        $this->maxPassenger=$gruz;
+        return $this->maxPassenger;
+    }
+    public function getmaxPassenger(){
+        return $this->maxPassenger;
+    }
 }
