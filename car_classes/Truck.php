c9a44df2baf6e61298908de7aaf4c4bda58625eb
Yulia N <iuliia_nak@gm.com>
2021-11-08 13:16:29 +0200

    Truck

--- car_classes/Truck.php
+++ car_classes/Truck.php
@@ -2,5 +2,17 @@
 
 class Truck extends NewCar
 {public const TYPE='Грузовики';
+    protected $maxGruz;//максимально возможный груз в кг;
 
+ public function transfer($gruz){
+     $this->setMaxGruz($gruz);
+     return self::TYPE . ": Максимально допустимый груз при грузоперевозке = " . $this->maxGruz."<br>";
+ }
+    public function setMaxGruz($gruz){
+        $this->maxGruz=$gruz;
+        return $this->maxGruz;
+    }
+    public function getMaxGruz(){
+        return $this->maxGruz;
+    }
 }
